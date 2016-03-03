7f29a4927ba5fe0bb5e67dcc5f33b0adb6de9396
Kasia <kkalinowskagorska@gmail.com>
2016-03-03 22:24:55 +0100

    Fix header

--- KKSwipeRevealGalleryView/UIView+RootView.swift
+++ KKSwipeRevealGalleryView/UIView+RootView.swift
@@ -1,6 +1,5 @@
 //
 //  UIView+RootView.swift
-//  KKSwipeRevealGalleryViewDemo
 //
 //  Created by Katarzyna Kalinowska-Górska on 21.02.2016.
 //  Copyright © 2016 kkalinowskagorska. All rights reserved.
